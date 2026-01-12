2e769079dc043e91fef2ce1a9bc93b5615ed9c37
BijleveldT <tbijleveld@clb.nl>
2026-01-12 14:39:15 +0100

    SS-3293 Code movement to 'prevent' local changes seen from root (5.2.release) branch

--- app/src/main/java/org/linphone/activities/main/sidemenu/fragments/SideMenuFragment.kt
+++ app/src/main/java/org/linphone/activities/main/sidemenu/fragments/SideMenuFragment.kt
@@ -84,12 +84,13 @@ class SideMenuFragment : GenericFragment<SideMenuFragmentBinding>() {
             override fun onAccountClicked(identity: String) {
                 Log.i("[Side Menu] Navigating to settings for account with identity: $identity")
 
+                sharedViewModel.toggleDrawerEvent.value = Event(true)
+                
                 // CLB: Block access to settings via AppConfig
                 if (ClbSettingsBlockChecker.AreSettingsBlocked(context)) {
                     return
                 }
 
-                sharedViewModel.toggleDrawerEvent.value = Event(true)
                 if (corePreferences.askForAccountPasswordToAccessSettings) {
                     showPasswordDialog(goToAccountSettings = true, accountIdentity = identity)
                 } else {
